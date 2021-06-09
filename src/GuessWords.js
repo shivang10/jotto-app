a2449151552c8e33608064b84d344dc645bd9032
Shivang Garg <shivang10garg@gmail.com>
2021-06-09 07:19:12 +0530

    add tests and code for non zero guessed words

--- src/GuessWords.js
+++ src/GuessWords.js
@@ -10,6 +10,26 @@ const GuessedWords = (props) => {
                 Try to guess the word
             </span>
         );
+    } else {
+        const guessedWordsRows = props.guessedWords.map((word, index) => (
+            <tr data-test="guessed-word" key={index}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+            </tr>
+        ))
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed words</h3>
+                <table>
+                    <thead>
+                        <tr><th>Guess</th><th>Matching Letters</th></tr>
+                    </thead>
+                    <tbody>
+                        {guessedWordsRows}
+                    </tbody>
+                </table>
+            </div>
+        )
     }
 
     return (
