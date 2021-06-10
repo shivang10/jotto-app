153e2cf3debb2a3225229dc5b9ca03d3768c1697
Shivang Garg <shivang10garg@gmail.com>
2021-06-10 07:54:15 +0530

    adds basic for app

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,13 @@
-test('testing will go in future', () => {
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../test/testUtils';
+import App from './App';
 
+const setup = () => {
+    return shallow(<App />);
+}
+
+test('testing will go in future', () => {
+    const wrapper = setup();
+    const appComponent = findByTestAttr(wrapper, 'component-app');
+    expect(appComponent).toHaveLength(1);
 });
