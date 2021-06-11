d38f45a0ba62a90a37d7bdbe3c6c4564aefdc5fd
Shivang Garg <shivang10garg@gmail.com>
2021-06-11 19:24:36 +0530

    adds success reducers tests

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,3 +1,13 @@
-export default (state, action) => {
-    return null;
+import { actionTypes } from "../actions";
+
+const successReducerFunction = (state = false, action) => {
+    switch (action.type) {
+        case (actionTypes.CORRECT_GUESS):
+            return true;
+
+        default:
+            return state;
+    }
 }
+
+export default successReducerFunction;
