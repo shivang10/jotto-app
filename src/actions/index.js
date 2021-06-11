2f48f8498882bce56b0429ed9305abc0fb2f27ac
Shivang Garg <shivang10garg@gmail.com>
2021-06-11 19:15:09 +0530

    adds correct guess

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
+export const actionTypes = {
+    CORRECT_GUESS: 'CORRECT_GUESS',
+}
+
+export function correctGuess() {
+    return { type: actionTypes.CORRECT_GUESS };
+}
+
 export const getSecretWord = () => {
     return axios.get('http://localhost:3030')
         .then(response => response.data)
