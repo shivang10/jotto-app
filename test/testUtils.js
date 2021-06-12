b9b4815a09bd520cc9d4ab9bfeaf630320bf8e38
Shivang Garg <shivang10garg@gmail.com>
2021-06-12 07:25:57 +0530

    adds guessWords tests

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,11 +1,11 @@
 /* eslint-disable react/forbid-foreign-prop-types */
 import checkPropTypes from "check-prop-types";
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../src/reducers';
-
+import { middlewares } from '../src/configureStore';
 
 export const storeFactory = (initialState) => {
-    createStore(rootReducer, initialState)
+    createStore(rootReducer, initialState, applyMiddleware(...middlewares))
 }
 
 export const findByTestAttr = (wrapper, val) => {
